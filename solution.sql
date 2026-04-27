e9f4c61710449d36bdfdebd74497d0baa68231dd
biblsc-beep <biblsc@farmingdale.edu>
2026-04-26 21:10:16 -0400

    Update solution.sql
    Signed-off-by: biblsc-beep <biblsc@farmingdale.edu>

--- solution.sql
+++ solution.sql
@@ -1,8 +1,2 @@
-SELECT artistname,
-       COUNT(*) AS total_streams
-FROM artists
-JOIN albums ON artists.artistid = albums.artistid
-JOIN songs ON albums.albumid = songs.albumid
-JOIN streams ON songs.songid = streams.songid
-GROUP BY artistname
-ORDER BY total_streams DESC;
+SELECT id, username
+FROM users;
